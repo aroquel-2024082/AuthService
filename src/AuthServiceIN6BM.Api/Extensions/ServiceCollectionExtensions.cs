f097879068e97c360640933cfd9f2c621e24825d
aroquel-2024082 <aroquel-2024082@kinal.edu.gt>
2026-02-02 11:38:43 -0600

    Avances de proyecto en clase

---
+++ src/AuthServiceIN6BM.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+using AuthServiceIN6BM.Domain.Interfaces;
+using AuthServiceIN6BM.Persistence.Data;
+using AuthServiceIN6BM.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using AuthServiceIN6BM.Application.Interfaces;
+
+namespace AuthServiceIN6BM.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(FileOptions =>
+            options.UserNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConventions());
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+
+        services.AddHealthChecks();
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
